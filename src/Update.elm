c904edd6c9eceb95e3f8f10b4f7a01556f64d4c8
Nate Nichols <lumastories@gmail.com>
2017-02-25 16:46:47 -0800

    consolidate Api with Update module

--- src/Update.elm
+++ src/Update.elm
@@ -1,10 +1,12 @@
 module Update exposing (update)
 
 import Model exposing (..)
-import Api
 import Http
 import Navigation
 import Port
+import Json.Decode as Decode
+import Json.Decode.Pipeline as JP
+import Json.Encode as Encode
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -26,7 +28,7 @@ update msg model =
         Login ->
             let
                 cmd =
-                    Http.send LoginResponse (Api.postCreds model)
+                    Http.send LoginResponse (postCreds model)
             in
                 ( { model | spin = True }, cmd )
 
@@ -50,7 +52,7 @@ update msg model =
                     case hitEnter of
                         True ->
                             if model.page == LoginPage then
-                                Http.send LoginResponse (Api.postCreds model)
+                                Http.send LoginResponse (postCreds model)
                             else
                                 Cmd.none
 
@@ -71,3 +73,69 @@ page hash =
 
         _ ->
             LoginPage
+
+
+
+-- Application Programing Interface
+-- Uniform Resource Locators
+-- REpresentational State Transfer
+-- IOW, talk to Daved's code :)
+
+
+authUrl : Model -> String
+authUrl model =
+    model.api ++ "/auth"
+
+
+defaultHeaders : Model -> List Http.Header
+defaultHeaders model =
+    let
+        headers =
+            [ Http.header "Accept" "application/json" ]
+
+        authHeaders =
+            case model.jwttoken.token of
+                "" ->
+                    headers
+
+                _ ->
+                    (Http.header "Authorization" ("Bearer " ++ model.jwttoken.token)) :: headers
+    in
+        authHeaders
+
+
+postCreds : Model -> Http.Request JwtToken
+postCreds model =
+    let
+        body =
+            userEncoder model |> Http.jsonBody
+
+        url =
+            authUrl model
+
+        decoder =
+            jwtDecoder
+    in
+        Http.request
+            { method = "POST"
+            , headers = defaultHeaders model
+            , url = url
+            , body = body
+            , expect = Http.expectJson decoder
+            , timeout = Nothing
+            , withCredentials = False
+            }
+
+
+userEncoder : Model -> Encode.Value
+userEncoder model =
+    Encode.object
+        [ ( "email", Encode.string model.email )
+        , ( "password", Encode.string model.password )
+        ]
+
+
+jwtDecoder : Decode.Decoder JwtToken
+jwtDecoder =
+    JP.decode JwtToken
+        |> JP.required "token" (Decode.string)
